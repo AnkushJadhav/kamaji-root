b7d47cbf6557912a80ba2724e512351abe2c1068
Ankush Jadhav <ankush@leadschool.in>
2020-05-20 17:05:00 +0530

    registration route added

--- pkg/server/http/routes.go
+++ pkg/server/http/routes.go
@@ -9,6 +9,7 @@ func attachExternalRoutes(srv *Server) {
 	srv.app.Get("/v1/api/users/:id", handlers.HandleGetUserByID(srv.config.StorageDriver))
 	srv.app.Post("/v1/api/users", handlers.HandleCreateUser(srv.config.StorageDriver))
 	srv.app.Delete("/v1/api/users/:id", handlers.HandleDeleteUser(srv.config.StorageDriver))
+	srv.app.Post("/v1/api/users/register/:id", handlers.HandleRegisterUser(srv.config.StorageDriver))
 }
 
 func attachInternalRoutes(srv *Server) {
