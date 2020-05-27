8232506457faefa446d6bafd98c9ccc774e7afe1
Ankush Jadhav <ankush@leadschool.in>
2020-05-27 13:46:40 +0530

    json encodings for users model

--- pkg/models/users.go
+++ pkg/models/users.go
@@ -13,11 +13,11 @@ const (
 
 // User represents a user in the system
 type User struct {
-	ID       string    `bson:"id"`
-	TS       time.Time `bson:"ts"`
-	Email    string    `bson:"email"`
-	Username string    `bson:"username,omitempty"`
-	Password string    `bson:"password,omitempty"`
-	RoleID   int       `bson:"roleId"`
-	Nodes    []Node    `bson:"nodes"`
+	ID       string    `bson:"id" json:"id"`
+	TS       time.Time `bson:"ts" json:"ts"`
+	Email    string    `bson:"email" json:"email"`
+	Username string    `bson:"username,omitempty" json:"username"`
+	Password string    `bson:"password,omitempty" json:"-"`
+	RoleID   int       `bson:"roleId" json:"role"`
+	Nodes    []Node    `bson:"nodes" json:"nodes"`
 }
